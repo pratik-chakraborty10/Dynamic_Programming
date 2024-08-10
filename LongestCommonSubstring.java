7e0eeda8aa17b14aeddd3507dac91b351f92ddef
Pratik Chakraborty <118681019+pratik-chakraborty10@users.noreply.github.com>
2024-08-10 20:59:46 +0530

    Add files via upload

---
+++ LongestCommonSubstring.java
@@ -0,0 +1,34 @@
+public class LongestCommonSubstring {
+
+    public static int LongestCommonSubstring(String str1,String str2){
+        int n=str1.length();
+        int m=str2.length();
+        int ans=0;
+        int dp[][]=new int[n+1][m+1];
+        //initialize the array
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                dp[i][j]=0;
+            }
+        }
+        //bottom up
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                    ans=Math.max(ans,dp[i][j]);
+                }else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String str1="abcde";
+        String str2="abgce";
+        System.out.println(LongestCommonSubstring(str1, str2));
+        
+    }
+}
